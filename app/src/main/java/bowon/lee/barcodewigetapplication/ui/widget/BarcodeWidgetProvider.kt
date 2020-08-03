e40419ce8c1b06fa289e0b6d498b2510ffc3d226
bowon lee <asdfg3678@gmail.com>
2020-08-03 23:21:57 +0900

    위젯 사이즈 지정

--- app/src/main/java/bowon/lee/barcodewigetapplication/ui/widget/BarcodeWidgetProvider.kt
+++ app/src/main/java/bowon/lee/barcodewigetapplication/ui/widget/BarcodeWidgetProvider.kt
@@ -8,6 +8,9 @@ import android.content.Intent
 import android.widget.RemoteViews
 import bowon.lee.barcodewigetapplication.MainActivity
 import bowon.lee.barcodewigetapplication.R
+import com.google.zxing.BarcodeFormat
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import kotlinx.android.synthetic.main.barcode_activity.*
 
 class BarcodeWidgetProvider : AppWidgetProvider(){
     override fun onUpdate(
@@ -16,6 +19,7 @@ class BarcodeWidgetProvider : AppWidgetProvider(){
         appWidgetIds: IntArray?
     ) {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
+
 
         // 해당 위젯을 클릭했을 때 특정 페이지로 이동시켜준다.
         val pendingIntent: PendingIntent = Intent(context, BarcodeActivity::class.java)
@@ -27,12 +31,27 @@ class BarcodeWidgetProvider : AppWidgetProvider(){
             context?.packageName,
             R.layout.barcode_widet
         ).apply {
-            setOnClickPendingIntent(R.id.barcode_text, pendingIntent)
+            setOnClickPendingIntent(R.id.widget_barcode_image, pendingIntent);
+            val barcodeNumber = "1234 5789 123456";
+            val barcodeEncoder = BarcodeEncoder();
+            val bitmap = barcodeEncoder.encodeBitmap(barcodeNumber, BarcodeFormat.CODE_128,500,160);
+
+            setImageViewBitmap(R.id.widget_barcode_image,bitmap);
+            setTextViewText(R.id.barcode_text,barcodeNumber);
         }
 
         appWidgetManager?.updateAppWidget(appWidgetIds, views)
     }
 
+    override fun onEnabled(context: Context?) {
+        super.onEnabled(context)
+
+    }
+
+    fun createBarcode(){
+
+
+    }
 
 
 }
